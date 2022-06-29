f4988978198fd9eed9534497c808fdad73f79602
root <69764760+root-7325@users.noreply.github.com>
2022-06-29 12:06:32 +0300

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,24 @@
+using System;
+class Shivanie drobi
+{
+    static void Main()
+    {
+        int a1;
+        int a2;
+        double a3 = 0;
+        double a4;
+        double a5;
+        Console.WriteLine("Input celoe chislo a potom ego drobnyu chast\n");
+        a1 = Convert.ToInt32(Console.ReadLine());
+        a2 = Convert.ToInt32(Console.ReadLine());
+        a4 = Convert.ToDouble(a2);
+        while (a4 > 0)
+        {
+            a3 = a4 / 100;
+            break;
+        }
+        a5 = a1 + a3;
+        Console.WriteLine(a5);
+        Console.ReadKey(true);
+    }
+}
