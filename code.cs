96db424b04a10a1e45d6ff8071c75314d4224b8c
root <69764760+root-7325@users.noreply.github.com>
2022-06-29 12:36:42 +0300

    Update code.cs

--- code.cs
+++ code.cs
@@ -3,22 +3,15 @@ class Shivanie
 {
     static void Main()
     {
-        int a1;
-        int a2;
-        double a3 = 0;
-        double a4;
-        double a5;
-        Console.WriteLine("Input celoe chislo a potom ego drobnyu chast\n");
-        a1 = Convert.ToInt32(Console.ReadLine());
-        a2 = Convert.ToInt32(Console.ReadLine());
-        a4 = Convert.ToDouble(a2);
-        while (a4 > 0)
-        {
-            a3 = a4 / 100; //1000?10000?100000
-            break;
-        }
-        a5 = a1 + a3;
-        Console.WriteLine(a5);
+
+        Console.WriteLine("Input int, then float number");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        double number4 = Convert.ToDouble(number2);
+        double number3 = 0;
+        while (number4 >= 1) number4 /= 10;
+        number3 = number4 + number;
+        Console.WriteLine(number3);
         Console.ReadKey(true);
     }
 }
